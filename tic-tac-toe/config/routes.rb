8c899e6a0f223adab4fdd7c72af646576f767fe1
Paul Duca <paul.r.duca@gmail.com>
2016-10-31 16:24:00 -0400

    working login/logout/create user

--- tic-tac-toe/config/routes.rb
+++ tic-tac-toe/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :new, :show]
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  root 'users#index'
 end
