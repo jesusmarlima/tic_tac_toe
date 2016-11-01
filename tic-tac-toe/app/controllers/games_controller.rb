a2dd094c3b2226dbbcaaa31a33a99ae558bac800
Amanda Hinchey <email": null>
2016-11-01 10:08:22 -0400

    Update routes for games

--- tic-tac-toe/app/controllers/games_controller.rb
+++ tic-tac-toe/app/controllers/games_controller.rb
@@ -1,13 +1,15 @@
 class GamesController < ApplicationController
 
   def show
+    @game = Game.find_by(id: params[:id])
   end
 
   def create
+    # byebug
     if @user = User.find_by(id: params[:user_id])
       @game = Game.new(user: @user)
       @game.save
-      render :show
+      redirect_to user_game_path(@user.id, @game.id)
     else
       @errors = ['You must be logged in']
       redirect_to welcome_path
